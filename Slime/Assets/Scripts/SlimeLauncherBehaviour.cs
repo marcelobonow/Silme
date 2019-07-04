8fc65a7587f7cf1f7b839844a8db2d43fa9700f8
Marcelo Bonow <marcelobonowott@hotmail.com>
2019-07-04 20:29:29 -0300

    Preview System not working correctly

---
+++ Slime/Assets/Scripts/SlimeLauncherBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LaunchState
+{
+    NotClicked,
+    Holding,
+    Flying,
+}
+
+
+public class SlimeLauncherBehaviour : MonoBehaviour
+{
+    private const int gizmosPreviewQuantity = 4;
+
+    [SerializeField] private Transform basePosition;
+    [SerializeField] private GameObject previewGizmoPrefab;
+
+    private LaunchState currentLaunchState;
+    private Vector2 initialClickPosition;
+    private List<GameObject> previewGizmos;
+
+    private void Awake()
+    {
+        initialClickPosition = Vector2.zero;
+        previewGizmos = new List<GameObject>();
+        for(var i = 0; i < gizmosPreviewQuantity; i++)
+        {
+            var gizmo = Instantiate(previewGizmoPrefab);
+            gizmo.SetActive(false);
+            previewGizmos.Add(gizmo);
+        }
+    }
+
+    public void OnPointerDown()
+    {
+        if(currentLaunchState == LaunchState.NotClicked)
+        {
+            currentLaunchState = LaunchState.Holding;
+            initialClickPosition = Input.mousePosition;
+        }
+    }
+    public void OnPointerUp() => currentLaunchState = LaunchState.NotClicked;
+
+    private void Update()
+    {
+        if(currentLaunchState == LaunchState.Holding)
+        {
+            UpdateGizmosPosition();
+        }
+        //Debug.Log((Vector2)Input.mousePosition - initialClickPosition);
+    }
+
+    private void UpdateGizmosPosition()
+    {
+        var force = Vector2.Distance(Input.mousePosition, initialClickPosition)/50f;
+        var angle = Mathf.Atan2(Input.mousePosition.x - initialClickPosition.x, Input.mousePosition.y-initialClickPosition.y) * Mathf.Deg2Rad;
+        //var angle = Vector2.SignedAngle(Input.mousePosition, initialClickPosition) * Mathf.Deg2Rad;
+        Debug.Log("Angulo: " + angle * Mathf.Rad2Deg + " graus");
+        for(var i = 0; i < gizmosPreviewQuantity; i++)
+        {
+            var gizmo = previewGizmos[i];
+            gizmo.SetActive(true);
+            gizmo.transform.position = GetGizmoPosition(force, angle, i * 0.25f);
+        }
+    }
+    private Vector2 GetGizmoPosition(float force, float angle, float time)
+    {
+        //x = x0 + vo.t
+        var x = basePosition.transform.position.x + force * Mathf.Cos(angle) * time;
+        //y = y0 + v0.sen(o)t.gt^2 / 2
+        var y = basePosition.transform.position.y + force * Mathf.Sin(angle) * time * Physics2D.gravity.y * time * time/2;
+        return new Vector2(x, y);
+    }
+}
